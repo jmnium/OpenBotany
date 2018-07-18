ce0fed1b6deeeb27d42e1725e13869a76e3486fa
Joe.Rogers <joe.rogers@titlemax.biz>
2018-07-18 10:37:42 -0500

    init check in of plant profile service

---
+++ plantService/src/main/java/com/mnium/plantService/PlantServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mnium.plantService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantServiceApplication.class, args);
+	}
+}
---
+++ plantService/src/test/java/com/mnium/plantService/PlantServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mnium.plantService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
